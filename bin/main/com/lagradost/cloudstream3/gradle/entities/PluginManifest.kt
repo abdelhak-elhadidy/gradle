7a324754621f672664b598b67b69e36afb5ad351
ApdelhakElhadidy <amsm.offical@gmail.com>
2022-11-17 16:59:54 +0200

    deleted:    bin/main/com/lagradost/cloudstream3/gradle/CloudstreamExtension.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/CloudstreamPlugin.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/DownloadUtils.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/Utils.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/configuration/ApkConfigurationProvider.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/configuration/Configurations.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/configuration/IConfigurationProvider.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/entities/PluginEntry.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/entities/PluginManifest.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/tasks/CleanCacheTask.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/tasks/CompileDexTask.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/tasks/CompileResourcesTask.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/tasks/DeployWithAdbTask.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/tasks/GenSourcesTask.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt 	deleted:    bin/main/com/lagradost/cloudstream3/gradle/tasks/Tasks.kt

--- bin/main/com/lagradost/cloudstream3/gradle/entities/PluginManifest.kt
+++
@@ -1,8 +0,0 @@
-package com.vodbot.egy.gradle.entities
-
-data class PluginManifest(
-    val pluginClassName: String?,
-    val name: String,
-    val version: Int,
-    val requiresResources: Boolean
-)
